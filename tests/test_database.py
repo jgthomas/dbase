97c0449f2b4f92d978cf561b238eef6e0c7beb92
James Thomas <codeactor@protonmail.com>
2017-08-24 22:56:17 +0100

    tests added

---
+++ tests/test_database.py
@@ -0,0 +1,28 @@
+import unittest
+from dbase import Database
+
+
+class DatabaseTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.db = Database("unit_test.db")
+
+    def setUp(self):
+        self.db.execute("CREATE TABLE test(
+                id  INTEGER PRIMARY KEY, 
+                val TEXT)")
+
+    @classmethod
+    def tearDownClass(self):
+        self.db.execute("DROP TABLE IF EXISTS test")
+
+    def test_insert_returns_last_row_id(self):
+        self.assertEqual(self.db.execute(
+            "INSERT INTO test(val) VALUES('one')"), 1)
+        self.assertEqual(self.db.execute(
+            "INSERT INTO test(val) VALUES('two')"), 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
