919aed8ae37c967eb9632fba48769c36e5bddc1a
James Thomas <codeactor@protonmail.com>
2017-08-24 23:47:59 +0100

    docstring on tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -21,10 +21,26 @@ class DatabaseTests(unittest.TestCase):
         self.db.execute("DROP TABLE test")
 
     def test_insert_returns_last_row_id(self):
+        """ INSERT returns id of last affected row. """
         self.assertEqual(self.db.execute(
             "INSERT INTO test(val) VALUES('one')"), 1)
         self.assertEqual(self.db.execute(
             "INSERT INTO test(val) VALUES('two')"), 2)
+
+    def test_select_all(self):
+        """ SELECT returns list, with each row as a dictionary. """
+        self.assertEqual(self.db.execute("SELECT * FROM test"), [])
+
+        rows = [
+            {"id": 1, "val": "foo"},
+            {"id": 2, "val": "bar"},
+            {"id": 3, "val": "baz"}
+        ]
+
+        for row in rows:
+            self.db.execute("INSERT INTO test(val) VALUES(?)", 
+                            row["val"])
+        self.assertEqual(self.db.execute("SELECT * FROM test"), rows)
 
 
 if __name__ == '__main__':
