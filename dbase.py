dad50db06392e5e5a3b97f341067b94e3f96d9d3
James Thomas <codeactor@protonmail.com>
2017-08-28 21:55:54 +0100

    sqlite shell added

--- dbase.py
+++ dbase.py
@@ -7,14 +7,19 @@ class Database:
     """
     Wrapper and utility methods for sqlite.
 
+    dbname  :  Name of the database file.
+    trace   :  An optional callback function to log or
+               print each SQL statement as executed.
+
     """
 
-    def __init__(self, dbname):
+    def __init__(self, dbname, trace=None):
         self.db = dbname
         try:
             self.conn = sqlite3.connect(self.db,
                                         detect_types=sqlite3.PARSE_DECLTYPES)
             self.conn.row_factory = sqlite3.Row
+            self.conn.set_trace_callback(trace)
             self.cur = self.conn.cursor()
         except sqlite3.Error:
             print("Error connecting to the database\n")
@@ -69,6 +74,46 @@ class Database:
                 dict_writer.writeheader()
                 dict_writer.writerows(rows)
 
+    def shell(self):
+        welcome = "\nSimple sqlite shell"
+        commands = ("\nBuild an SQL statement, or:\n\n"
+                    "'quit'  : exit shell\n"
+                    "'help'  : show this message\n"
+                    "'clear' : reset statement buffer\n"
+                    "'state' : show statement buffer\n")
+        prompt = "{0}> ".format(self.db)
+        statement = ""
+        print(welcome)
+        print(commands)
+        while True:
+            line = input(prompt).lower()
+            if line == "quit":
+                break
+            if line == "help":
+                print(commands)
+                continue
+            if line == "clear":
+                statement = ""
+                continue
+            if line == "state":
+                print(statement)
+                continue
+            statement = " ".join([statement, line])
+            if sqlite3.complete_statement(statement):
+                try:
+                    statement = statement.strip()
+                    if re.search(r"^\s*(?:DELETE|UPDATE|INSERT|DROP|CREATE|ALTER)",
+                                 statement, re.IGNORECASE):
+                                     print("'SELECT' statements only in the terminal")
+                                     continue
+                    elif statement.upper().startswith("SELECT"):
+                        print(self.execute(statement))
+                    else:
+                        print(commands)
+                except sqlite3.Error as e:
+                    print("An error occurred:", e.args[0])
+                statement = ""
+
 
 class IterableTable:
    """ 
@@ -87,8 +132,19 @@ class IterableTable:
        return self._db.columns(self.table)
 
    def __getitem__(self, item):
-        if self.exists_table:
-            statement = "SELECT * FROM {0} LIMIT ?, 1".format(self.table)
-            self._db.cur.execute(statement, (item,))
-            row = self._db.cur.fetchone()
-            return dict(row)
+       if self.exists_table:
+           statement = "SELECT * FROM {0} LIMIT ?, 1".format(self.table)
+           self._db.cur.execute(statement, (item,))
+           row = self._db.cur.fetchone()
+           if row is not None:
+               return dict(row)
+           raise StopIteration
+
+   def take(self, n):
+       try:
+           return [self.__getitem__(i) for i in range(0, n)]
+       except StopIteration as exc:
+           raise IndexError("IterableTable index out of range") from exc
+
+   def head(self):
+       return self.take(1)
