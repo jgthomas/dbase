c9c8b233fd0060b9adf08645bec8a7aadbdbb363
James Thomas <codeactor@protonmail.com>
2017-08-29 07:57:46 +0100

    all four main operations now have tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -8,45 +8,49 @@ class DatabaseTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.db = Database("unit_test.db")
+        cls.rows = [
+            {"id": 1, "val": "foo"},
+            {"id": 2, "val": "bar"},
+            {"id": 3, "val": "baz"}
+        ]
 
     def setUp(self):
         self.db.execute("""CREATE TABLE test(
                 id  INTEGER PRIMARY KEY, 
                 val TEXT)""")
 
+    def test_select_returns_list_of_row_as_dicts(self):
+        self.assertEqual(self.db.execute("SELECT * FROM test"), [])
+        for row in self.rows:
+            self.db.execute("INSERT INTO test(val) VALUES(?)", row["val"])
+        self.assertEqual(self.db.execute(
+            "SELECT * FROM test"), self.rows)
+
     def test_insert_returns_last_row_id(self):
-        """ INSERT returns id of last affected row. """
         self.assertEqual(self.db.execute(
             "INSERT INTO test(val) VALUES('one')"), 1)
         self.assertEqual(self.db.execute(
             "INSERT INTO test(val) VALUES('two')"), 2)
 
-    def test_select_all(self):
-        """ SELECT returns list, with each row as a dictionary. """
+    def test_update_returns_affected_row_count(self):
         self.assertEqual(self.db.execute("SELECT * FROM test"), [])
+        for row in self.rows:
+            self.db.execute("INSERT INTO test(val) VALUES(?)", row["val"])
+        self.assertEqual(self.db.execute(
+            "UPDATE test SET val=? WHERE id>?", 'puff', 1), 2)
 
-        rows = [
-            {"id": 1, "val": "foo"},
-            {"id": 2, "val": "bar"},
-            {"id": 3, "val": "baz"}
-        ]
-
-        for row in rows:
-            self.db.execute("INSERT INTO test(val) VALUES(?)", 
-                            row["val"])
-        self.assertEqual(self.db.execute("SELECT * FROM test"), rows)
+    def test_delete_returns_affected_row_count(self):
+        self.assertEqual(self.db.execute("SELECT * FROM test"), [])
+        for row in self.rows:
+            self.db.execute("INSERT INTO test(val) VALUES(?)", row["val"])
+        self.assertEqual(self.db.execute(
+            "DELETE FROM test WHERE id>?", 1), 2)
 
     def test_exists_table(self):
         """ Return True if the table exists, else False. """
-        self.assertEquals(self.db.exists_table("test"), True)
-        self.assertEquals(self.db.exists_table("not_test"), False)
+        self.assertEqual(self.db.exists_table("test"), True)
+        self.assertEqual(self.db.exists_table("not_test"), False)
 
-    def test_columns(self):
-        """ FIX: for table that exists but has no data. """
-        columns = ("id", "val")
-        self.assertEquals(self.db.columns("test"), columns)
-        self.assertEquals(self.db.columns("not_test"), None)
-    
     def tearDown(self):
         self.db.execute("DROP TABLE test")
 
