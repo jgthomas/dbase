f8553664c79c1f1f0a1b7847ccbfb0cd5cc45667
James Thomas <codeactor@protonmail.com>
2017-08-30 20:16:14 +0100

    iter_table tests added

---
+++ tests/test_iterabletable.py
@@ -0,0 +1,44 @@
+import unittest
+import os
+from dbase import Database
+from iter_table import IterableTable
+
+
+class IterableTableTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.db = Database("unit_test.db")
+        cls.db.execute("""CREATE TABLE test(
+                id  INTEGER PRIMARY KEY, 
+                name TEXT,
+                age INTEGER)""")
+        cls.rows = [
+                {"id": 1, "name": "dog", "age": 10},
+                {"id": 2, "name": "cat", "age": 20},
+                {"id": 3, "name": "man", "age": 30},
+                {"id": 4, "name": "rat", "age": None}
+        ]
+        for row in cls.rows:
+            cls.db.execute("INSERT INTO test(name, age) VALUES(?, ?)",
+                            row["name"], row["age"])
+        cls.table = IterableTable("unit_test.db", "test")
+
+    def test_take_items_from_table(self):
+        taken = [
+                  {"id": 1, "name": "dog", "age": 10},
+                  {"id": 2, "name": "cat", "age": 20}
+                ]
+        self.assertEqual(self.table.take(2), taken)
+
+    def test_take_beyond_range(self):
+        self.assertRaises((StopIteration, IndexError), self.table.take(10))
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.db.execute("DROP TABLE IF EXISTS test")
+        os.remove("unit_test.db")
+
+
+if __name__ == '__main__':
+    unittest.main()
