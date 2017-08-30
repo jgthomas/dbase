f5419a747b58e0e46194ddb849e83beba8445602
James Thomas <codeactor@protonmail.com>
2017-08-30 06:21:10 +0100

    iterabletable in own module

---
+++ itertable.py
@@ -0,0 +1,38 @@
+
+
+from dbase import Database
+
+
+class IterableTable:
+    """
+    Turns a database table into an iterable.
+
+    """
+
+    def __init__(self, dbname, table):
+        self._db = Database(dbname)
+        self.table = table
+
+    def exists_table(self):
+        return self._db.exists_table(self.table)
+
+    def columns(self):
+        return self._db.columns(self.table)
+
+    def __getitem__(self, item):
+        if self.exists_table:
+            statement = "SELECT * FROM {0} LIMIT ?, 1".format(self.table)
+            self._db.cur.execute(statement, (item,))
+            row = self._db.cur.fetchone()
+            if row is not None:
+                return dict(row)
+            raise StopIteration
+
+    def take(self, n):
+        try:
+            return [self.__getitem__(i) for i in range(0, n)]
+        except StopIteration as exc:
+            raise IndexError("IterableTable index out of range") from exc
+
+    def head(self):
+        return self.take(1)
